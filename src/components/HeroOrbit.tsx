b76ec4e18d1f7d23dc5ba98d7b653552eb91dde7
ayomahansani <ayohansi17@gmail.com>
2025-03-24 14:44:57 +0530

    create HeroOrbit tsx file

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,18 +1,27 @@
 import {PropsWithChildren} from "react";
 
-export const HeroOrbit = ({children, size} : PropsWithChildren<{ size: number}>) => {
+export const HeroOrbit = ({
+    children,
+    size,
+    rotation
+} : PropsWithChildren<{ size: number; rotation: number;}>) => {
     return (
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
             <div
-                className="border border-red-500 size-[718px]"
+                className="flex items-start justify-start"
                 style={{
+                    transform: `rotate(${rotation}deg)`,
                     height:`${size}px`,
                     width:`${size}px`,
             }}
             >
-                <div className="border border-red-500 inline-flex">
-                    {children}
-                </div>
+            <div className="inline-flex"
+                 style={{
+                    transform: `rotate(${rotation * -1}deg)`,
+                 }}
+            >
+                {children}
+            </div>
             </div>
         </div>
     )
