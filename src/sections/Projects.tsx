305098cafc50c4d748ea8e8b2864be5f8c1f472b
ayomahansani <ayohansi17@gmail.com>
2025-03-24 21:47:25 +0530

    add SectionHeader component to projects section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import CheckCircleIcon from "@assets/icons/check-circle.svg";
 import ArrowUpRightIcon from "@assets/icons/arrow-up-right.svg";
 import grainImage from "@assets/images/grain.jpg";
+import {SectionHeader} from "@/components/SectionHeader";
 
 const portfolioProjects = [
   {
@@ -49,13 +50,7 @@ export const ProjectsSection = () => {
   return (
       <section className="pb-16 lg:py-24">
         <div className="container">
-          <div className="flex justify-center">
-            <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text text-center">
-              Real World Result
-            </p>
-          </div>
-          <h2 className="font-serif text-3xl text-center mt-6 md:text-5xl">Featured Projects</h2>
-          <p className="text-center text-white/60 mt-4 md:text-lg max-w-md mx-auto lg:text-xl">See how I transformed concepts into engaging digital experiences.</p>
+          <SectionHeader eyebrow="Real World Result" title="Featured Projects" description="See how I transformed concepts into engaging digital experiences."/>
           <div className="flex flex-col gap-20 mt-10 md:mt-20">
             {portfolioProjects.map(project => (
                 <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden
