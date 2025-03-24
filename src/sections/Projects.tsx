58934fdc8990c6788be9400dc73ec24f8b496b80
ayomahansani <ayohansi17@gmail.com>
2025-03-24 15:30:39 +0530

    create cards for each projects

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,6 +1,7 @@
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import Image from "next/image";
 
 const portfolioProjects = [
   {
@@ -42,5 +43,43 @@ const portfolioProjects = [
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return (
+      <div>
+        <div className="container">
+          <div className="flex justify-center">
+            <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text text-center">
+              Real World Result
+            </p>
+          </div>
+          <h2 className="font-serif text-3xl text-center mt-6">Featured Projects</h2>
+          <p className="text-center text-white/60 mt-4">See how I transformed concepts into engaging digital experiences.</p>
+          <div className="flex flex-col mt-10">
+            {portfolioProjects.map(project => (
+                <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden
+                after:-z-10 after:content-[''] after:absolute after:inset-0
+                after:outline after:outline-2 after:-outline-offset-2 after:rounded-3xl after:outline-white/20">
+                  <div className="flex">
+                    <div className="inline-flex gap-2 bg-gradient-to-r from-emerald-300 to-sky-400 font-bold uppercase tracking-widest text-sm">
+                      <span>{project.company}</span>
+                      <span>&bull;</span>
+                      <span>{project.year}</span>
+                    </div>
+                  </div>
+                  <h3>{project.title}</h3>
+                  <hr />
+                  <ul>
+                    {project.results.map((result) => (
+                        <li>{result.title}</li>
+                    ))}
+                  </ul>
+                  <a href={project.link}>
+                    <button>View Live Site</button>
+                  </a>
+                  <Image src={project.image} alt={project.title} />
+                </div>
+            ))}
+          </div>
+        </div>
+      </div>
+  );
 };
