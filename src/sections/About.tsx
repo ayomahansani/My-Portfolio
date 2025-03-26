3c7959e08005fb95679c3dde5a2c86961b70ea80
ayomahansani <ayohansi17@gmail.com>
2025-03-26 14:13:51 +0530

    make responsive abut section for mobile and tab

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -90,46 +90,48 @@ export const AboutSection = () => {
           <div className="container">
               <SectionHeader title="A Glimps Into My World" eyebrow="About Me" description="Learn more about who I am, what I do and what inspires me." />
               <div className="mt-20 flex flex-col gap-8">
-                  <Card className="h-[320px] p-0">
+                  <Card className="h-[320px]">
                       <CardHeader
                           title="My Toolbox"
                           description="Explore the technologies and tools I use to craft exceptional digital experiences."
-                          className="px-6 pt-6"
+                          className=""
                       />
-                      <ToolBoxItems items={toolBoxItems} className="mt-6"/>
+                      <ToolBoxItems items={toolBoxItems} className=""/>
                       <ToolBoxItems
                           items={toolBoxItems}
                           className="mt-6"
                           itemsWrapperClassName="-translate-x-1/2"
                       />
                   </Card>
-                  <Card className="h-[320px] p-0 flex flex-col">
-                      <CardHeader className="px-6 py-6" title="Beyond the Code" description="Explore my interests and hobbies beyond the digital realm." />
-                      <div className="relative flex-1">
-                          {hobbies.map(hobby => (
-                              <div key={hobby.title}
-                                   className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
-                                   style={{
-                                       left: hobby.left,
-                                       top: hobby.top,
-                                    }}
-                              >
-                                  <span className="font-medium text-gray-950">{hobby.title}</span>
-                                  <span>{hobby.emoji}</span>
-                              </div>
-                          ))}
-                      </div>
-                  </Card>
-                  <Card className="h-[320px] p-0 relative">
-                      <Image src={mapImage} alt={"map"} className="h-full w-full object-cover object-left-top"/>
-                      <div
-                          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20
+                  <div className="grid grid-cols-1 md:grid-cols-5 gap-8">
+                      <Card className="h-[320px] p-0 flex flex-col col-span-3">
+                          <CardHeader className="px-6 py-6" title="Beyond the Code" description="Explore my interests and hobbies beyond the digital realm." />
+                          <div className="relative flex-1">
+                              {hobbies.map(hobby => (
+                                  <div key={hobby.title}
+                                       className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+                                       style={{
+                                           left: hobby.left,
+                                           top: hobby.top,
+                                       }}
+                                  >
+                                      <span className="font-medium text-gray-950">{hobby.title}</span>
+                                      <span>{hobby.emoji}</span>
+                                  </div>
+                              ))}
+                          </div>
+                      </Card>
+                      <Card className="h-[320px] p-0 relative col-span-2">
+                          <Image src={mapImage} alt={"map"} className="h-full w-full object-cover object-left-top"/>
+                          <div
+                              className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20
                           rounded-full bg-gradient-to-r from-emerald-300 to-sky-400
                           after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:outline-offset-2 after:rounded-full after:outline-gray-950/30"
-                      >
-                          <Image src={memojiGirl} alt={"girl memoji"} className="size-20"/>
-                      </div>
-                  </Card>
+                          >
+                              <Image src={memojiGirl} alt={"girl memoji"} className="size-20"/>
+                          </div>
+                      </Card>
+                  </div>
               </div>
           </div>
       </div>
