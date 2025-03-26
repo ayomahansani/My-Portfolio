4282f503ee3ca86a85936a53b1b0306d80cb37aa
ayomahansani <ayohansi17@gmail.com>
2025-03-26 13:58:03 +0530

    finish creating about section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,13 +1,11 @@
 import {SectionHeader} from "@/components/SectionHeader";
 import {Card} from "@/components/Card";
-import StarIcon from "@assets/icons/star.svg";
 import JavascriptIcon from "@assets/icons/square-js.svg";
 import HTMLIcon from "@assets/icons/html5.svg";
 import CSSIcon from "@assets/icons/css3.svg";
 import ReactIcon from "@assets/icons/react.svg";
 import ChromeIcon from "@assets/icons/chrome.svg";
 import GithubIcon from "@assets/icons/github.svg";
-import {TechIcon} from "@/components/TechIcon";
 import mapImage from "@assets/images/map.png";
 import Image from "next/image";
 import memojiGirl from "@assets/images/memoji-avatar-4.png";
@@ -44,38 +42,52 @@ const toolBoxItems = [
 const hobbies = [
     {
         title: 'Painting',
-        emoji: '🎨'
+        emoji: '🎨',
+        left: '5%',
+        top: '5%',
     },
     {
         title: 'Photography',
-        emoji: '📷'
-    },
-    {
-        title: 'Gaming',
-        emoji: '🎮'
+        emoji: '📷',
+        left: '50%',
+        top: '5%',
     },
     {
         title: 'Hiking',
-        emoji: '🧗🏽'
+        emoji: '🧗🏽',
+        left: '35%',
+        top: '40%',
+    },
+    {
+        title: 'Gaming',
+        emoji: '🎮',
+        left: '10%',
+        top: '35%',
     },
     {
         title: 'Music',
-        emoji: '🎶'
+        emoji: '🎶',
+        left: '70%',
+        top: '45%',
     },
     {
         title: 'Fitness',
-        emoji: '🏋🏼‍♀️'
+        emoji: '🏋🏼‍♀️',
+        left: '5%',
+        top: '65%',
     },
     {
         title: 'Reading',
-        emoji: '‍📚️'
+        emoji: '‍📚️',
+        left: '45%',
+        top: '70%',
     }
 ]
 
 export const AboutSection = () => {
   return (
       <div className="py-20">
-          <div>
+          <div className="container">
               <SectionHeader title="A Glimps Into My World" eyebrow="About Me" description="Learn more about who I am, what I do and what inspires me." />
               <div className="mt-20 flex flex-col gap-8">
                   <Card className="h-[320px] p-0">
@@ -91,20 +103,32 @@ export const AboutSection = () => {
                           itemsWrapperClassName="-translate-x-1/2"
                       />
                   </Card>
-                  <Card>
-                      <CardHeader title="Beyond the Code" description="Explore my interests and hobbies beyond the digital realm." />
-                      <div>
+                  <Card className="h-[320px] p-0 flex flex-col">
+                      <CardHeader className="px-6 py-6" title="Beyond the Code" description="Explore my interests and hobbies beyond the digital realm." />
+                      <div className="relative flex-1">
                           {hobbies.map(hobby => (
-                              <div key={hobby.title}>
-                                  <span>{hobby.title}</span>
+                              <div key={hobby.title}
+                                   className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+                                   style={{
+                                       left: hobby.left,
+                                       top: hobby.top,
+                                    }}
+                              >
+                                  <span className="font-medium text-gray-950">{hobby.title}</span>
                                   <span>{hobby.emoji}</span>
                               </div>
                           ))}
                       </div>
                   </Card>
-                  <Card>
-                      <Image src={mapImage} alt={"map"} />
-                      <Image src={memojiGirl} alt={"girl memoji"} />
+                  <Card className="h-[320px] p-0 relative">
+                      <Image src={mapImage} alt={"map"} className="h-full w-full object-cover object-left-top"/>
+                      <div
+                          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20
+                          rounded-full bg-gradient-to-r from-emerald-300 to-sky-400
+                          after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:outline-offset-2 after:rounded-full after:outline-gray-950/30"
+                      >
+                          <Image src={memojiGirl} alt={"girl memoji"} className="size-20"/>
+                      </div>
                   </Card>
               </div>
           </div>
