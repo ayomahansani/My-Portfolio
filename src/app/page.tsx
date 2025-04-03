e690b0c8af9a2068dc40d9ba32cab850545259e1
ayomahansani <ayohansi17@gmail.com>
2025-04-03 09:56:46 +0530

    update some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
 import { SkillsSection } from "@/sections/Skills";
 import { Footer } from "@/sections/Footer";
+import { AboutMeSection} from "@/sections/AboutMe";
 
 export default function Home() {
     return (
         <div>
             <Header />
             <HeroSection />
+            <AboutMeSection />
             <ProjectsSection />
             <TapeSection />
             <SkillsSection />
